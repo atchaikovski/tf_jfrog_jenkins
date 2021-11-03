31f4148acdadc49d5b2f93cecf80fc8f04ea2e85
Alex Tchaikovski <alex@tchaikovski.net>
2021-11-03 11:18:34 +0300

    removed ssl certs

--- install_artifactory.sh
+++ install_artifactory.sh
@@ -8,9 +8,7 @@ sudo yum install -y -q s3fs-fuse
 export JFROG_HOME=/opt/jfrog
 sudo sh -c "echo 'JFROG_HOME=/opt/jfrog' >>/etc/profile"
 export S3_BIN=/opt/s3
-export S3_CERTS=/opt/certs
 export S3_BUCKET=sf-project4-binaries
-export S3_CERTS_BUCKET=tchaikovski-link-letsencrypt-certificates
 
 ## here should be a var passed from Terraform to make this universal!
 export JFROG_URL="jfrog.tchaikovski.link"
@@ -24,16 +22,13 @@ sudo sh -c "echo 'artifactory ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers.d/90-cloud-
 
 # preparing to mount S3 and copy files
 sudo mkdir $S3_BIN
-sudo mkdir $S3_CERTS
 sudo s3fs $S3_BUCKET -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 $S3_BIN
-sudo s3fs $S3_CERTS_BUCKET -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 $S3_CERTS
 
 # check if there's anything in S3 and copy/extract/install
 if [[ -f "$S3_BIN/jfrog-rpm-installer.tar.gz" ]] 
 then
     echo "==> files of Artifactory found, proceed"
     sudo cp $S3_BIN/jfrog-rpm-installer.tar.gz /opt
-    sudo cp $S3_CERTS/ssl-certs/* ~/
 
     cd /opt
     sudo tar zxvf jfrog-rpm-installer.tar.gz
@@ -57,10 +52,13 @@ then
 
     echo "==> removing secrets and unwanted stuff..."
     sudo umount $S3_BIN
-    sudo rm -rf $S3_BIN   
+    sudo umount $S3_CERTS
+    sudo rm -rf $S3_BIN  
     sudo rm /etc/passwd-s3fs
     sudo rm -rf /home/centos/s3fs-*
     sudo rm $ARTIFACTORY_HOME/artifactory.rpm
+    sudo rm $JFROG_HOME/xray/xray.rpm
+
   else
 
     echo "==> S3 location with JFrog binaries not found.. fix your S3 access"
