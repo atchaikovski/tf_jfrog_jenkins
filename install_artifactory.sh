96eb4aab42a8c21d6c55b917741910480d32e044
Alex Tchaikovski <alex@tchaikovski.net>
2021-11-03 09:37:10 +0300

    031121 - artifactory certs for https

--- install_artifactory.sh
+++ install_artifactory.sh
@@ -9,13 +9,14 @@ export JFROG_HOME=/opt/jfrog
 sudo sh -c "echo 'JFROG_HOME=/opt/jfrog' >>/etc/profile"
 export S3_BIN=/opt/s3
 export S3_BUCKET=sf-project4-binaries
+export S3_CERTS_BUCKET="tchaikovski-link-letsencrypt-certificates"
 
 ## here should be a var passed from Terraform to make this universal!
 export JFROG_URL="jfrog.tchaikovski.link"
 sudo sh -c "echo 'JFROG_URL=jfrog.tchaikovski.link' >>/etc/profile"
 
 export ARTIFACTORY_HOME=$JFROG_HOME/artifactory
-sudo sh -c "echo '' >>/etc/profile"
+sudo sh -c "echo 'ARTIFACTORY_HOME=$JFROG_HOME/artifactory' >>/etc/profile"
 
 sudo adduser artifactory
 sudo sh -c "echo 'artifactory ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers.d/90-cloud-init-users"
@@ -23,12 +24,15 @@ sudo sh -c "echo 'artifactory ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers.d/90-cloud-
 # preparing to mount S3 and copy files
 sudo mkdir $S3_BIN
 sudo s3fs $S3_BUCKET -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 $S3_BIN
+sudo s3fs $S3_CERTS_BUCKET -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 $S3_BIN
 
 # check if there's anything in S3 and copy/extract/install
 if [[ -f "$S3_BIN/jfrog-rpm-installer.tar.gz" ]] 
 then
     echo "==> files of Artifactory found, proceed"
     sudo cp $S3_BIN/jfrog-rpm-installer.tar.gz /opt
+    sudo cp $S3_BIN/ssl-certs/* ~/
+
     cd /opt
     sudo tar zxvf jfrog-rpm-installer.tar.gz
     sudo mv jfrog-platform-trial-prox-7.27.6-rpm jfrog
@@ -46,8 +50,6 @@ then
     sudo systemctl start artifactory.service
     sudo systemctl start xray.service
 
-    sleep 15
-
     echo "==> removing jfrog archive"
     sudo rm /opt/jfrog-rpm-installer.tar.gz
 
@@ -56,7 +58,7 @@ then
     sudo rm -rf $S3_BIN   
     sudo rm /etc/passwd-s3fs
     sudo rm -rf /home/centos/s3fs-*
-
+    sudo rm $ARTIFACTORY_HOME/artifactory.rpm
   else
 
     echo "==> S3 location with JFrog binaries not found.. fix your S3 access"
--- main.tf
+++ main.tf
@@ -70,8 +70,8 @@ resource "aws_instance" "artifactory_server" {
       "chmod 0600 passwd-s3fs",
       "sudo mv passwd-s3fs /etc/passwd-s3fs",
       "sudo chown root:root /etc/passwd-s3fs",
-      "chmod +x install_artifactory.sh"
-#      "./install_artifactory.sh"
+      "chmod +x install_artifactory.sh",
+      "./install_artifactory.sh"
     ]
   }
 
